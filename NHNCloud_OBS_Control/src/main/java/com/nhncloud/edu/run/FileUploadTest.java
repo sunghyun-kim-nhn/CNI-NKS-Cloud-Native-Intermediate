d7d0d6bdd9020ce191608cc83a68269873e1da83
김성현/인프라플랫폼개발팀/CL <sunghyun.kim@nhn.com>
2024-01-08 14:31:54 +0900

    빌드를 위한 수정

--- NHNCloud_OBS_Control/src/main/java/com/nhncloud/edu/run/FileUploadTest.java
+++ NHNCloud_OBS_Control/src/main/java/com/nhncloud/edu/run/FileUploadTest.java
@@ -3,7 +3,6 @@ package com.nhncloud.edu.run;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class FileUploadTest {
 
     public static void main(String[] args) {
